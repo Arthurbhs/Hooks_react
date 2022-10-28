d3196a7be6103540f56b2d380428a2ec9e431d84
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-10-28 16:27:35 -0300

    mudando estado de input

---
+++ UseStateFinal.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+
+const UseState = (props) => {
+    // esta função hook permite alterar os valores  das variaveis
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+
+    return (
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!"
+            />
+            <SectionTitle title="ex#1"/>
+            <div className="center">
+                <span className="text">{count}</span>
+               <div className='button'>
+               <button className="btn"
+                 onClick={() => setCount(count - 2)}>-2</button>
+                  <button className="btn"
+                 onClick={() => setCount(count - 10)}>-10</button>
+                <button className="btn"
+               onClick={() => setCount(count - 1)}>-1</button>
+
+                <button className="btn"
+                 onClick={() => setCount(count + 1)}>+1</button>
+
+<button className="btn"
+                 onClick={() => setCount(count + 2)}>+2</button>
+
+<button className="btn"
+                 onClick={() => setCount(count + 10)}>+10</button>
+                </div> 
+            </div>
+            <SectionTitle title="ex#2"/>
+            <input type="text" className="input"
+             value={name} onChange={e => setName(e.target.value)}/>
+        </div>
+    )
+}
+
+export default UseState
