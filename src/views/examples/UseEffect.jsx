6d3e15e48d0c3975aadf302a6a8115e396212d73
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-10-28 15:18:11 -0300

    adição e subtração com useStates

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -10,40 +10,6 @@ function calcFatorial(num){
 }
 
 const UseEffect = (props) => {
-    const [number, setNumber] = useState(0)
-    const [fatorial, setFatorial] = useState(1)
-  
-    useEffect(function(){
-        setFatorial(calcFatorial(number))
-
-    }, [number])
-
-    useEffect(function () {
-      if(fatorial > 100000){
-        document.title = "Pra que agredir"
-      }
-
-    }, [fatorial] )
-
-    useEffect(function () {
-        if(fatorial <  0 ){
-          document.title = "ai que burro"
-        }
-  
-      }, [fatorial] )
-
-      useEffect(function () {
-        if((fatorial <  50) && (fatorial > 0)  ){
-          document.title = "lhe falta odio"
-        }
-  
-      }, [fatorial] )
-
-      const [status, setStatus] = useState("impar")
-
-      useEffect(function() {
-           setStatus(number % 2 === 0 ? "par" : "impar")
-      }, [number])
 
 
 
@@ -52,23 +18,7 @@ const UseEffect = (props) => {
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-          
-            
-            />
-            <SectionTitle title="ex #1"/>
-              <div className="center">
-                <div>
-                    <span className="text">Fatorial</span>
-                      <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
-                </div>
-                  <input type="number" className="input"
-                  value={number}
-                  onChange={e => setNumber(e.target.value)}/>
-
-                </div>
-
-              
-            
+         />
         </div>
     )
 }
